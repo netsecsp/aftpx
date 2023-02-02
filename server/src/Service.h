b5699509b7b744d6359840cf599559c430df2e44
yangshengqian <yangshengqian@chinasofti.com>
2023-02-02 12:04:00 +0800

    1.3.2-20230202

--- server/src/Service.h
+++ server/src/Service.h
@@ -96,7 +96,7 @@ public:
         }
 
         CComPtr<IThreadPool> threadpool;
-        m_spInstanceManager->NewInstance(0, 0, IID_IThreadPool, (void**)&threadpool);
+        m_spInstanceManager->NewInstance(0, 1, IID_IThreadPool, (void**)&threadpool);
 
         PORT tcpport = (PORT)m_setsfile.get_long("tcp", "port", 21);
         if( tcpport )
